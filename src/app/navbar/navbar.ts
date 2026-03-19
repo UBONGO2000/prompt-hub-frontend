10953c84a8cad82ce71ec8bdee437bcbd7b48dc8
UBONGO2000 <gadyubongo@gmail.com>
2026-03-19 10:54:43 +0100

    ajout des routes pour la navigation html

--- src/app/navbar/navbar.ts
+++ src/app/navbar/navbar.ts
@@ -1,9 +1,10 @@
 import { Component, inject } from '@angular/core'
 import { ThemeService } from '../theme.service'
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './navbar.html',
   styleUrl: './navbar.scss',
 })
